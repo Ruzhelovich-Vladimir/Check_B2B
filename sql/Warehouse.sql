c0ce4983e841817a02cb1469208b18a9b95b53d7
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2021-02-08 13:52:22 +0600

    V 1.0

--- sql/Warehouse.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-SupplierId [Саплайр]
-, W_ID [ИД]
-, ExternalCode [КОД УС]
-, WarehouseName [Наименование]
-, AllowTareReturn [Возвратная тара] 
-FROM Warehouses w 
-where w.supplierid=@supplier_id and w.status=2
